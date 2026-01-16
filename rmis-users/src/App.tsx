f623eaf869cae8852b16d5657fc9a8170401bd3c
gregwilke <gwilke@psmfc.org>
2026-01-16 15:40:41 -0800

    Switch to HashRouter for GitHub Pages compatibility

--- rmis-users/src/App.tsx
+++ rmis-users/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
 import Auth from "@/pages/Auth";
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/sandbox/rmis-users/dist">
+      <HashRouter>
         <Routes>
           <Route path="/auth" element={<Auth />} />
           <Route
@@ -24,7 +24,7 @@ function App() {
           <Route path="/" element={<Navigate to="/users" replace />} />
         </Routes>
         <Toaster />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   );
 }
