65397cc47fdd2b53960b0c9e5464bcc1feeb19bc
gregwilke <gwilke@psmfc.org>
2026-01-16 15:44:14 -0800

    Fix dark mode CSS by adding safelist and index.html to content

--- rmis-users/tailwind.config.ts
+++ rmis-users/tailwind.config.ts
@@ -7,7 +7,9 @@ export default {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./index.html",
   ],
+  safelist: ["dark"],
   prefix: "",
   theme: {
     container: {
